c159c94d109e8d88923b469f54267e2d51240103
jimbobding <jimmygrahamdavid@gmail.com>
2023-04-19 15:53:17 +0100

    addeed 404 error, full crud

--- src/main/java/com/example/demo/rest/CatController.java
+++ src/main/java/com/example/demo/rest/CatController.java
@@ -14,53 +14,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import domain.Cat;
+import com.example.demo.domain.Cat;
+import com.example.demo.service.CatService;
 
 @RestController
 public class CatController {
 	@GetMapping("/")
 	public String greeting() {
 		return "Hello World";
-		
-		
 	}
-	List<Cat>cats = new ArrayList<>();
+
+	List<Cat> cats = new ArrayList<>();
+
+	private CatService service;
+
+	public CatController(CatService service) {
+		super();
+		this.service = service;
+	}
+
 	@PostMapping("/create")
 	public ResponseEntity<Cat> createCat(@RequestBody Cat c) {
-		this.cats.add(c); 
-		Cat created = this.cats.get(cats.size() -1);
-		return  new ResponseEntity<>(created, HttpStatus.CREATED );
+
+		Cat created = this.service.createCat(c);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
 	}
+
 	@GetMapping("/getAll")
-	public List<Cat>getAll(){
-		return this.cats;
+	public List<Cat> getAll() {
+		return this.service.getAll();
 	}
-	
+
 	@GetMapping("/get/{id}")
-	public Cat get(@PathVariable int id){
-		return this.cats.get(id);
+	public Cat get(@PathVariable int id) {
+		return this.service.getById(id);
 	}
-	
-	@DeleteMapping("/remove/{id}") 
+
+	@DeleteMapping("/remove/{id}")
 	public Cat remove(@PathVariable int id) {
-		return this.cats.remove(id);
+		return this.service.delete(id);
 	}
-	
+
 	@PatchMapping("/update/{id}")
-	public Cat update(@PathVariable int id,
-			 		 @RequestParam(name="name", required = false) String name,
-					 @RequestParam(name="hasWhiskers", required = false) Boolean hasWhiskers, 
-					 @RequestParam(name="evil", required = false) Boolean evil, @RequestParam(name="length", required = false) Integer length) {
-		
-					Cat toUpdate = this.cats.get(id);
-							if(name != null)toUpdate.setName(name);
-							if(hasWhiskers != null)toUpdate.setHasWhiskers(hasWhiskers);
-							if(evil != null)toUpdate.setEvil(evil);
-							if(length != null)toUpdate.setLength(length);
-							
-						return this.cats.get(id);
-		
+	public Cat update(@PathVariable int id, @RequestParam(name = "name", required = false) String name,
+			@RequestParam(name = "hasWhiskers", required = false) Boolean hasWhiskers,
+			@RequestParam(name = "evil", required = false) Boolean evil,
+			@RequestParam(name = "length", required = false) Integer length) {
+
+		return this.service.getById(id);
+
 	}
-	
-	
+
 }
